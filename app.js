0c5a30ebbfd8a9b07c975f54ef95955d86a18c56
yunayoun <nsook91@naver.com>
2022-08-16 19:00:18 +0900

    first1

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require("express");           
-const app = express();                      
-app.set("view engine", "ejs");                 
-app.use("/static", express.static("static")) ;
-app.use('/static', express.static('public'));
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use("/static", express.static("static"));
 
 const port = 8000;
 
-app.get('/',(req, res) => {
-    res.render('test',{});
+app.get("/", (req, res) => {
+  res.render("test", {});
 });
-app.get('/',(req, res) => {
-    res.render('Home',{});
+app.get("/", (req, res) => {
+  res.render("Home", {});
 });
-app.listen(port, ()=>{
-    console.log("server open: ", port);
+app.get("/one", (req, res) => {
+  res.render("one", {});
+});
+
+app.listen(port, () => {
+  console.log("server open: ", port);
 });
